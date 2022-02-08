dd0bf448a9d1bff969a3a4ee1141ee818d98b0fa
Robert Bach <robert.bach@kiwigrid.com>
2022-02-08 08:47:11 +0100

    Use fixed local build of resteasy-reactive-common-processor; Duplicate interface annotations to implementation signature as a workaround for annotation parameter being null in ParameterConverterProvider

--- src/main/java/org/acme/TestResource.java
+++ src/main/java/org/acme/TestResource.java
@@ -3,11 +3,12 @@ package org.acme;
 import java.util.Map;
 
 import io.smallrye.mutiny.Uni;
+import javax.ws.rs.QueryParam;
 
 public class TestResource implements MyApi {
 
 	@Override
-	public Uni<Pojo> testEndpoint(Map<String, Object> projection) {
+	public Uni<Pojo> testEndpoint(@QueryParam("testqueryparam") Map<String, Object> projection) {
 		Pojo pojo = new Pojo();
 		pojo.field = projection.get("a").toString();
 		return Uni.createFrom().item(pojo);
